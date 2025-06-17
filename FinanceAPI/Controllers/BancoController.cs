3308dfd951f1e10cdfc93ad47a51748df9358817
Kaique Cidrini <Kaique.Cidrini@contractor.rhimagnesita.com>
2025-06-17 15:52:49 -0300

    First commit

---
+++ FinanceAPI/Controllers/BancoController.cs
@@ -0,0 +1,76 @@
+﻿using FinanceAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BancoController : ControllerBase
+    {
+        public BancoController()
+        {
+
+        }
+
+        [HttpGet("{bankName}")]
+        public string Get([FromRoute] string bankName)
+        {
+            List<string> bankList = new()
+            {
+               "Itau",
+               "Nubank"
+            };
+            if (bankList.Contains(bankName))
+            {
+                return bankName;
+            } else
+            {
+                return ("Valor Invalido");
+            }
+        }
+        [HttpGet]
+        public List<string> GetAll()
+        {
+            List<string> bankList = new()
+            {
+               "Itau",
+               "Nubank"
+            };
+            return bankList;
+        }
+        [HttpDelete("{bankName}")]
+        public List<string> Delete([FromRoute] string bankName)
+        {
+            List<string> bankList = new()
+            {
+               "Itau",
+               "Nubank"
+            };
+            bankList.Remove(bankName);
+            return bankList;
+        }
+        [HttpPost]
+        public List<BancoModel> Create([FromBody] BancoModel model)
+        {
+            List<BancoModel> bancoList = new();
+            bancoList.Add(model);
+            return bancoList;
+        }
+        [HttpPut("{id}")]
+        public List<BancoModel> Update([FromBody] BancoModel bancoName, [FromRoute] int id)
+        {
+            List<BancoModel> bancoList = new List<BancoModel>();
+            bancoList.Add(new BancoModel(1, "Nubank"));
+            bancoList.Add(new BancoModel(2, "Itau"));
+            bancoList.Add(new BancoModel(3, "Inter"));
+
+            var oldName = bancoList.Find(b => b.Id == id);
+            if (oldName == null)
+                return [];
+
+            oldName.Name = bancoName.Name;
+            return bancoList;
+        }
+
+    }
+}
---
+++ FinanceAPI/Controllers/UserController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Finance.API.Controllers.UserController;
+
+namespace Finance.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        [HttpPost]
+        public List<User> Create([FromBody] User user)
+        {
+            // Simula uma lista de usuários já cadastrados
+            List<User> userList = new();
+
+            // Adiciona um novo usuário na lista
+            userList.Add(user);
+            
+            // Retorna a lista atualizada
+            return userList;
+        }
+
+        [HttpPut("{id}")]
+        public List<User> Update([FromBody] User user, [FromRoute] int id)
+        {
+            List<User> userList = [
+                new(){Id = 1, Name = "Lucas"},
+                new(){Id = 2, Name = "João"},
+                new(){Id = 3, Name = "Nautinha"}
+            ];
+
+            var oldUser = userList.Find(u => u.Id == id);
+
+            if (oldUser is null) return [];
+
+            oldUser.Name = user.Name;
+               
+            return userList;
+
+        }
+
+
+        [HttpGet]
+        public List<string> GetAll()
+        {
+            List<string> userNameList =
+            [
+                "Lucas",
+                "Kaique"
+            ];
+
+            return userNameList;
+        }
+
+        [HttpDelete("{userName}")]
+        public List<string> Delete([FromRoute] string userName)
+        {
+            List<string> userNameList =
+            [
+                "Lucas",
+                "Kaique"
+            ];
+
+            userNameList.Remove(userName);
+
+            return userNameList;
+        }
+    }
+}
